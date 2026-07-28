9757c65d01339ba3b9ab5c60f3c2e62135e1f054
jackkurian044-maker <jackkurian044@gmail.com>
2026-07-28 19:55:24 +0530

    Update Next.js

--- Next.js
+++ Next.js
@@ -1,1 +1,9 @@
 npx create-next-app@latest . --typescript --tailwind --eslint --app
+✔ TypeScript?               Yes
+✔ ESLint?                   Yes
+✔ Tailwind CSS?             Yes
+✔ src/ directory?           No
+✔ App Router?               Yes
+✔ Turbopack?                Yes
+✔ Customize import alias?   No
+  
